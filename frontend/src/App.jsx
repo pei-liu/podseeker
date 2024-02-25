57fd114df35899347e093f8018b8e41f2a61a0ca
Pei Liu <peiliu@Peis-M1-MBP.local>
2024-02-25 17:18:46 -0500

    Css formatting and add empty state

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import Episode from "./Episode";
 
 const App = (props) => {
   const [searchTerm, setSearchTerm] = useState(props.search_term);
+  const [emptyListMsg, setEmptyListMsg] = useState("Submit a search term to begin. Try 'tech'!");
   const [episodes, setEpisodes] = useState([]);
 
   const handleSearchTermChange = (e) => {
@@ -13,12 +14,24 @@ const App = (props) => {
   const handleSearch = async (e) => {
     e.preventDefault();
 
+    if(searchTerm === undefined) {
+      setEmptyListMsg("Input can't be blank. Submit a search term to begin. Try 'tech'!");
+      return;
+    }
+
     try {
       const response = await fetch(`/search_episodes?q=${encodeURIComponent(searchTerm)}`);
       if (!response.ok) {
         throw new Error('Network response was not ok');
       }
+
       const data = await response.json();
+
+      if(data.length === 0) {
+        setEmptyListMsg(`0 results for '${searchTerm}'. Try another search term.`);
+      } else {
+        setEmptyListMsg('');
+      }
 
       setEpisodes(data);
     } catch (error) {
@@ -29,15 +42,17 @@ const App = (props) => {
 
   return (
     <div>
+      <h1 id='logo'>PodSeeker</h1>
       <h3>Hello! Enter a keyword to search across many podcast series for episodes that match your interest.</h3>
-      <hr />
-      <form onSubmit={handleSearch}>
+      <form onSubmit={handleSearch} id='search-form'>
         <label htmlFor="name">
           Enter Keyword:
-          <input id="name" type="text" value={searchTerm} onChange={handleSearchTermChange} />
         </label>
+        <input type="text" value={searchTerm} onChange={handleSearchTermChange} />
         <button type="submit">Search</button>
       </form>
+      <hr />
+      <p id="empty-state">{emptyListMsg}</p>
       <div id="episode-list-container">
         {
           episodes.map((ep, index) => (
